e5b7037c7cacfde6b64c50ab82d09da41302614b
Tran Duc Canh <levitran1102005@gmail.com>
2026-05-08 20:38:23 +0700

    feat: apply @Async to NotificationService.send()

--- src/main/java/com/project/notificationservice/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/project/notificationservice/service/impl/NotificationServiceImpl.java
@@ -2,14 +2,15 @@ package com.project.notificationservice.service.impl;
 
 import com.project.notificationservice.domain.entity.Notification;
 import com.project.notificationservice.domain.enums.Channel;
+import com.project.notificationservice.exception.BaseException;
+import com.project.notificationservice.exception.ErrorCode;
 import com.project.notificationservice.sender.NotificationSender;
 import com.project.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -17,10 +18,10 @@ public class NotificationServiceImpl implements NotificationService {
     private final Map<Channel, NotificationSender> senderMap;
 
     public NotificationServiceImpl(List<NotificationSender> senders) {
-        this.senderMap = senders.stream()
-                .collect(Collectors.toMap(NotificationSender::getChannel, sender -> sender));
+        this.senderMap = senders.stream().collect(Collectors.toMap(NotificationSender::getChannel, sender -> sender));
     }
 
+    @Async("notificationExecutor")
     @Override
     public void send(Notification notification) {
         NotificationSender sender = senderMap.get(notification.getChannel());
